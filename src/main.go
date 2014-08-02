5cee5f567dca979528c78d3f3cec90c21c622d15
lmliang <lmliang@live.cn>
2014-08-02 22:21:28 +0800

    complete code need debug

--- src/main.go
+++ src/main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"session"
+	_ "session/Provider/memory"
 )
 
 var globalSessions *session.Manager
 
 func init() {
-	globalSessions = session.NewManager("memory", "globalSessionId", 3600)
+	globalSessions, _ = session.NewManager("memory", "globalSessionId", 3600)
 
 	go globalSessions.GC()
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	r.ParseForm()
+	if r.Method == "GET" {
+		str := "Welcome Back " + sess.SessionID()
+		io.WriteString(w, str)
+	} else {
+		str := "Sadness " + sess.SessionID()
+		io.WriteString(w, str)
+	}
+}
+
+func main() {
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Listen failed")
+	}
 }
